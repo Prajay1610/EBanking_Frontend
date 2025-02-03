b3bcb3c4e3ff6822eda15ffa0e335ebf4f63e711
Prajay Chavan <prajaypc16@gmail.com>
2025-02-03 19:45:28 +0530

    View All Banks Integeration done

--- Frontend/src/services/adminService.js
+++ Frontend/src/services/adminService.js
@@ -13,3 +13,28 @@ export const addNewAdmin = async(reqbody)=>{
         toast.error('Error occured while adding new admin');
     }
 }
+
+
+export const addNewBank = async(reqbody)=>{
+    const url = createUrl('bank/add');
+    console.log("reqbody"+JSON.stringify(reqbody));
+    try {
+        const response = await axios.post(url,reqbody);
+        console.log("resp data"+JSON.stringify(response.data));
+        return response.data;
+    } catch (error) {
+        toast.error('Error occured while adding new bank');
+    }
+}
+
+
+export const getAllBankManagers=async(reqbody)=>{
+    const url = createUrl('admin/getAllBankManagersFromUser');
+    try {
+        const response = await axios.get(url);
+        console.log("resp data"+JSON.stringify(response.data));
+        return response.data;
+    } catch (error) {
+        toast.error('Error occured while fetching bank managers');
+    }
+}
