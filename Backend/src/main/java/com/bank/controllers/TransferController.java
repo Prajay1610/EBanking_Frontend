068a7790d2b8d3c161cbbf5de2845ee7da7f5fce
Prajay Chavan <prajaypc16@gmail.com>
2025-02-05 00:56:48 +0530

    Transfer Money Integerated

--- Backend/src/main/java/com/bank/controllers/TransferController.java
+++ Backend/src/main/java/com/bank/controllers/TransferController.java
@@ -2,6 +2,7 @@ package com.bank.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.bank.dtos.TransferMoneyDto;
 import com.bank.services.TransferService;
-
+@CrossOrigin(origins="http://localhost:3000")
 @RestController
 @RequestMapping("/transfer")
 public class TransferController {
