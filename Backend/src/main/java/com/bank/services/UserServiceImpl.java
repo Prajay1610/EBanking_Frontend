f18fde74c6373864d29f36a8838a71c5b53f3c9c
Shubham Kakde <79091229+ShuBOY622@users.noreply.github.com>
2025-02-05 10:28:35 +0530

    add Customer route complete, added option to create account in the same form

--- Backend/src/main/java/com/bank/services/UserServiceImpl.java
+++ Backend/src/main/java/com/bank/services/UserServiceImpl.java
@@ -7,10 +7,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.bank.entities.AccountType;
+import com.bank.entities.BankAccount;
 import com.bank.entities.Customer;
 import com.bank.entities.Role;
 import com.bank.entities.User;
 import com.bank.exception.ResourceNotFoundException;
+import com.bank.repositories.BankAccountRepository;
+import com.bank.repositories.BankRepository;
 import com.bank.repositories.CustomerRepository;
 import com.bank.repositories.UserRepository;
 
@@ -26,6 +30,13 @@ public class UserServiceImpl implements UserService {
 	
 	@Autowired
 	private CustomerRepository customerRepository;
+	
+	@Autowired
+	private BankAccountRepository bankAccRepository;
+	
+	@Autowired
+	private BankRepository bankRepo;
+	
 	@Override
 	public User authenticate(String email, String password) {
 		return userRepository.findByEmail(email).filter(u->u.getPassword().equals(password)).orElseThrow(()->new ResourceNotFoundException("User not found with email : "+email));
@@ -37,7 +48,16 @@ public class UserServiceImpl implements UserService {
 		{
 			Customer c = new Customer(user);
 			customerRepository.save(c);
+			
+				BankAccount newBankAc = new BankAccount();
+				newBankAc.setCustomer(c);
+				newBankAc.setAccountType(user.getAccountType());
+				newBankAc.setBank(bankRepo.findById(user.getBankId()).get());
+				
+				bankAccRepository.save(newBankAc);
+	
 		}
+		
 		return persistentUser;
 	}
 	
